270f87b77fba78fa0731cb5b169aa29b740f90ff
Jeffrey G Berry <48kmem@gmail.com>
2022-06-02 16:27:08 -0500

    Completed excerise to 3.3 - need to get to 3.6

--- index.js
+++ index.js
@@ -1,41 +1,48 @@
 const express = require('express')
 const app = express()
+let date_time = new Date()
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2022-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2022-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2022-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]
+let persons = [
+    { 
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
+      },
+      { 
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+      },
+      { 
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+      },
+      { 
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+      }
+    ]
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+    response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
+app.get('/info', (request, response) => {
+    response.send(`Phonebook has info for ${persons.length} people.` + '\n' + `${date_time}`)
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
+    const person = persons.find(person => person.id === id)
     
-    if (note) {
-      response.json(note)
+    if (person) {
+      response.json(person)
     } else {
       response.status(404).end()
     }
