ad4b35b278a55e1e30908fe1d856c58111271a0b
Jeffrey G Berry <48kmem@gmail.com>
2022-06-06 10:32:30 -0500

    Refined app.post and added sepaerate ID generator

--- index.js
+++ index.js
@@ -30,6 +30,16 @@ let persons = [
       }
     ]
 
+/********** ID Generator *********/
+const generateId = () => {
+  const maxId =  persons.length > 0
+  ? Math.max(...persons.map(n => n.id))
+  : 0
+
+  return maxId + 1
+}
+/*********************************/
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
@@ -44,10 +54,24 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.post('api/persons', (request, response) => {
-  const person = request.body
-  console.log(person)
+  const body = request.body
+
+  if(!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const person = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
   response.json(person)
 })
+/*********************************/
 
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
@@ -58,7 +82,7 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
       response.status(404).end()
     }
-  })
+})
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
